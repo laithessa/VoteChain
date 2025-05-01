0504b9be7fd7dc4ecff716edd5f226e29a628412
Kiran Kumar V <imkirankumar23@gmail.com>
2025-05-01 13:19:46 +0530

    Update 2_deploy_contracts.js

--- contracts/2_deploy_contracts.js
+++ contracts/2_deploy_contracts.js
@@ -1,5 +1,8 @@
-var Voting = artifacts.require("./Voting.sol")
+// Import the Voting contract artifact, which is the compiled version of the Voting.sol Solidity contract.
+var Voting = artifacts.require("./Voting.sol");
 
+// Export a function that specifies the deployment process for the Voting contract.
 module.exports = function(deployer) {
-  deployer.deploy(Voting)
-}
+  // Use the deployer object to deploy the Voting contract to the blockchain.
+  deployer.deploy(Voting);
+};
